d16ac8e2ce5514552f8d590e154ab49486455f04
network <brayyy316@gmail.com>
2026-02-21 19:13:13 -0500

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -20,11 +20,17 @@ if ("serviceWorker" in navigator && !/localhost/.test(window.location.toString()
 
 export const queryClient = new QueryClient;
 
-export type Page = { default: ElementType, path: string, caseSensitive?: boolean };
-const pages = import.meta.globEager<Page>("./src/pages/*.tsx");
+// ✅ FIXED: Proper TypeScript typing for globEager
+type PageModule = { default: ElementType };
+const modules = import.meta.globEager<PageModule>("./src/pages/*.tsx");
+const pages = Object.entries(modules).map(([path, mod]) => ({
+  default: mod.default,
+  path: path.replace('./src/pages', '').replace(/\.tsx?$/, '').substring(1),
+  caseSensitive: false
+}));
 
 // Log SHS GAMES!!
-console.log(" _______           _______    _______  _______  _______  _______  _______ \n(  ____ \\|\\     /|(  ____ \\  (  ____ \\(  ___  )(       )(  ____ \\(  ____ \\\n| (    \\/| )   ( || (    \\/  | (    \\/| (   ) || () () || (    \\/| (    \\/\n| (_____ | (___) || (_____   | |      | (___) || || || || (__    | (_____ \n(_____  )|  ___  |(_____  )  | | ____ |  ___  || |(_)| ||  __)   (_____  )\n      ) || (   ) |      ) |  | | \\_  )| (   ) || |   | || (            ) |\n/\\____) || )   ( |/\\____) |  | (___) || )   ( || )   ( || (____/\\/\\____) |\n\\_______)|/     \\|\\_______)  (_______)|/     \\||/     \\|(_______/\\_______)");
+console.log(" _______           _______    _______  _______  _______  _______  _______ \\n(  ____ \\\\|\\\\     /|(  ____ \\\\  (  ____ \\\\(  ___  )(       )(  ____ \\\\(  ____ \\\\\\n| (    \\\\/| )   ( || (    \\\\/  | (    \\\\/| (   ) || () () || (    \\\\/| (    \\\\/\\n| (_____ | (___) || (_____   | |      | (___) || || || || (__    | (_____ \\n(_____  )|  ___  |(_____  )  | | ____ |  ___  || |(_)| ||  __)   (_____  )\\n      ) || (   ) |      ) |  | | \\\\_  )| (   ) || |   | || (            ) |\\n/\\\\____) || )   ( |/\\\\____) |  | (___) || )   ( || )   ( || (____/\\\\/\\\\____) |\\n\\\\_______)|/     \\\\|\\\\_______)  (_______)|/     \\\\||/     \\\\|(_______/\\\\_______)");
 console.log("%cJoin our cult at http://github.com/SHSGames/shsgames.github.io", "color: #1976d4");
 console.log("%cHi, Evan!", "font-style: italic");
 
@@ -36,10 +42,10 @@ ReactDOM.render(
 					<Toolbar/>
 					<Drawer/>
 					<Routes>
-						{ Object.values(pages).map((page, key) => <Route
+						{ pages.map((page, key) => <Route
 							key={ key }
-							path={ base + page.path.substring(1) }
-							caseSensitive={ page.caseSensitive || false }
+							path={ base + page.path }
+							caseSensitive={ page.caseSensitive }
 							element={ <page.default/> }/>
 						) }
 					</Routes>
@@ -53,3 +59,4 @@ ReactDOM.render(
 	</StrictMode>,
 	document.getElementById("root")
 );
+
